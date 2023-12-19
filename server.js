cda6cf4dd8a02d714e8f16ac1b2f908dd73c1c53
Hernán Baravalle <hernan.baravalle@gmail.com>
2023-12-20 00:27:22 +0100

    Este es nuestro primer commit

---
+++ server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const routes = require("./routes");
+const APP_PORT = process.env.APP_PORT || 3000;
+const app = express();
+
+// app.use(cors()); // Para habilitar esta línea es necesario instalar la librería `cors`.
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Nuestra prueba de deploy..."));
+routes(app);
+
+app.listen(APP_PORT, () => {
+  console.log(`\n[Express] Servidor corriendo en el puerto ${APP_PORT}.`);
+  console.log(`[Express] Ingresar a http://localhost:${APP_PORT}.\n`);
+});
