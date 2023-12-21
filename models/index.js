27f071115c438fcee8b11f971a895fec4d1327ee
Hernán Baravalle <hernan.baravalle@gmail.com>
2023-12-21 02:15:12 +0100

    Fix dialect module

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST, // Ej: 127.0.0.1
     dialect: process.env.DB_CONNECTION, // Ej: mysql
-    dialectModule: require(process.env.DB_DIALECT_MODULE),
+    dialectModule: require("pg"),
     logging: false, // Para que no aparezcan mensajes en consola.
   },
 );
