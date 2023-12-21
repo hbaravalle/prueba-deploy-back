075f495bac7b3e2e7c8137fe38913f5aafa381da
Hernán Baravalle <hernan.baravalle@gmail.com>
2023-12-21 01:52:42 +0100

    Test 1

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
-const cors = require("cors");
+// const cors = require("cors");
 const express = require("express");
 const routes = require("./routes");
 const APP_PORT = process.env.APP_PORT || 3000;
 const app = express();
 
-app.use(cors()); // Para habilitar esta línea es necesario instalar la librería `cors`.
+// app.use(cors());
+console.log(APP_PORT);
 app.use(express.static("public"));
 app.use(express.json());
 
